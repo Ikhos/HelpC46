45d825724f012cb5acd5a1c6c58c64962a9d75ea
Ikhos <73320135+Ikhos@users.noreply.github.com>
2021-03-01 20:15:04 -0600

    Add files via upload

---
+++ apples.js
@@ -0,0 +1,12 @@
+class Apple {
+    constructor() {
+        this.y = random(400, 600);
+        this.x = 1500;
+        this.sprite = createSprite(this.x, this.y);
+        this.sprite.addImage(appleImage);
+        this.sprite.velocityX = -4;
+        this.sprite.lifetime = 1500;
+        this.sprite.scale = 0.1;
+    }
+  
+}
